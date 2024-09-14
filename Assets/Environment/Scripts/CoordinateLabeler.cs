ba5dc66af260ab03e60daa67a2234ca3c851a22c
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-09-15 00:15:14 +0800

    4. More Debugging Tools

--- Assets/Environment/Scripts/CoordinateLabeler.cs
+++ Assets/Environment/Scripts/CoordinateLabeler.cs
@@ -11,17 +11,19 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, .5f, 0f);
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
+    GridManager gridManager;
 
-    Waypoint waypoint;
 
     void Awake() 
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
-        waypoint = GetComponentInParent<Waypoint>();
     }
 
     void Update()
@@ -45,14 +47,28 @@ public class CoordinateLabeler : MonoBehaviour
     }
     void SetLabelColor()
     {
-        if (waypoint.IsPlaceable)
-        {
-            label.color = defaultColor;
-        }
-        else
+        if (gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null) { return; }
+
+        if (!node.isWalkable)
         {
             label.color = blockColor;
         }
+        else if (!node.isPath)
+        {
+            label.color = exploredColor;
+        }
+        else if (!node.isExplored)
+        {
+            label.color = exploredColor;
+        }
+        else {
+            label.color = defaultColor;
+        }
+
     }
 
     void DisplayCoordinates()
