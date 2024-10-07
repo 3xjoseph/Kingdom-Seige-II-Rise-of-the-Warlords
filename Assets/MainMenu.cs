1212fc33ec0fe33c48482ad2f5cc41b07c96a909
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-10-08 01:42:59 +0800

    Final Build

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    int currentSceneIndex;
+    public void StartGame()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) 
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+   public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exited the Game");
+    } 
+}
